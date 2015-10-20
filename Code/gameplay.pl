d615cf008a897cf83e19d3e47e0b4ec2a49a39df
mariomacedo <mario.macedo@ludustechnology.com>
2015-10-20 19:02:39 +0100

    check adding pieces redone

--- Code/gameplay.pl
+++ Code/gameplay.pl
@@ -1,9 +1,14 @@
 :- [board].
 %:- [moves].
 
+:- use_module(library(lists)).
+:- use_module(library(clpfd)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  GAMEPLAY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 start(BoardState):-
-    emptyBoard(BoardState),
-    printBoard(BoardState).
+    emptyBoard(BoardState), nl,
+    printBoard(BoardState),
+    reverse(BoardState, Inv),
+    nl, nl, nl,
+    printBoard(Inv).
--- Code/moves.pl
+++ Code/moves.pl
@@ -1,4 +1,5 @@
-:- use_l
+:- use_module(library(lists)).
+:- use_module(library(clpfd)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ADD PIECE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -10,107 +11,157 @@ checkInRow([H|T], Row, Index, Column):-
 	checkInRow(T, Row, Index2, Column).
 
 
-checkMiddleCell(Row, Column):- %top left cell
-	checkInRow(BoardState, Row-1, 0, Column-1). 
-checkMiddleCell(Row, Column):- %top right cell
-	checkInRow(BoardState, Row-1, 0, Column). 
-checkMiddleCell(Row, Column):- %right cell
-	checkInRow(BoardState, Row, 0, Column+1). 
-checkMiddleCell(Row, Column):- %bottom right cell
-	checkInRow(BoardState, Row+1, 0, Column+1). 
-checkMiddleCell(Row, Column):- %bottom left cell
-	checkInRow(BoardState, Row+1, 0, Column). 
-checkMiddleCell(Row, Column):- %left cell
-	checkInRow(BoardState, Row, 0, Column-1). 
+checkMiddleCell(Board, Row, Column):- %top left cell
+	checkInRow(Board, Row-1, 0, Column-1). 
+checkMiddleCell(Board, Row, Column):- %top right cell
+	checkInRow(Board, Row-1, 0, Column). 
+checkMiddleCell(Board, Row, Column):- %right cell
+	checkInRow(Board, Row, 0, Column+1). 
+checkMiddleCell(Board, Row, Column):- %bottom right cell
+	checkInRow(Board, Row+1, 0, Column+1). 
+checkMiddleCell(Board, Row, Column):- %bottom left cell
+	checkInRow(Board, Row+1, 0, Column). 
+checkMiddleCell(Board, Row, Column):- %left cell
+	checkInRow(Board, Row, 0, Column-1). 
 
 
 
+%===============CORNER CHECK RUELS=================
 isCorner(R, C, CornerNumber):-
-	R = 0,
+	R = 0, !,
 	C = 0,
 	CornerNumber is 1.
 isCorner(R, C, CornerNumber):-
-	R = 0,
+	R = 0, !,
 	C = 4,
 	CornerNumber is 2.
-isCorner(R, C, CornerNumber):-
-	R = 4,
-	C = 8,
-	CornerNumber is 3.
-isCorner(R, C, CornerNumber):-
-	R = 8,
-	C = 4,
-	CornerNumber is 4.
-isCorner(R, C, CornerNumber):-
-	R = 8,
-	C = 0,
-	CornerNumber is 5.
-isCorner(R, C, CornerNumber):-
-	R = 4,
-	C = 0,
-	CornerNumber is 6.
 isCorner(_, _, CornerNumber):-
 	fail.
 
-checkCorner(1):-
-	checkInRow(BoardState, 0, 0, 1). %right cell
-checkCorner(1):-
-	checkInRow(BoardState, 1, 0, 0). %bottom left cell
-checkCorner(1):-
-	checkInRow(BoardState, 1, 0, 1). %bottom right cell
-checkCorner(2):-
-	checkInRow(BoardState, 0, 0, 3). %left cell
-checkCorner(2):-
-	checkInRow(BoardState, 1, 0, 4). %bottom left cell
-checkCorner(2):-
-	checkInRow(BoardState, 1, 0, 5). %bottom right cell
-checkCorner(3):-
-	checkInRow(BoardState, 4, 0, 7). %left cell
-checkCorner(3):-
-	checkInRow(BoardState, 3, 0, 7). %top left cell
-checkCorner(3):-
-	checkInRow(BoardState, 5, 0, 7). %bottom left cell
-checkCorner(4):-
-	checkInRow(BoardState, 8, 0, 3). %left cell
-checkCorner(4):-
-	checkInRow(BoardState, 7, 0, 4). %top left cell
-checkCorner(4):-
-	checkInRow(BoardState, 7, 0, 5). %top right cell
-checkCorner(5):-
-	checkInRow(BoardState, 8, 0, 1). %right cell
-checkCorner(5):-
-	checkInRow(BoardState, 7, 0, 0). %top left cell
-checkCorner(5):-
-	checkInRow(BoardState, 7, 0, 1). %top right cell
-checkCorner(6):-
-	checkInRow(BoardState, 4, 0, 1). %right cell
-checkCorner(6):-
-	checkInRow(BoardState, 3, 0, 0). %bottom left cell
-checkCorner(6):-
-	checkInRow(BoardState, 5, 0, 0). %bottom right cell
+checkCorner(Board, 1):-
+	checkInRow(Board, 0, 0, 1). %right cell
+checkCorner(Board, 1):-
+	checkInRow(Board, 1, 0, 0). %bottom left cell
+checkCorner(Board, 1):-
+	checkInRow(Board, 1, 0, 1). %bottom right cell
+checkCorner(Board, 2):-
+	checkInRow(Board, 0, 0, 3). %left cell
+checkCorner(Board, 2):-
+	checkInRow(Board, 1, 0, 4). %bottom left cell
+checkCorner(Board, 2):-
+	checkInRow(Board, 1, 0, 5). %bottom right cell
+checkCorner(Board, 3):-
+	checkInRow(Board, 4, 0, 7). %left cell
+checkCorner(Board, 3):-
+	checkInRow(Board, 3, 0, 7). %top left cell
+checkCorner(Board, 3):-
+	checkInRow(Board, 5, 0, 7). %bottom left cell
+checkCorner(Board, 6):-
+	checkInRow(Board, 4, 0, 1). %right cell
+checkCorner(Board, 6):-
+	checkInRow(Board, 3, 0, 0). %bottom left cell
+checkCorner(Board, 6):-
+	checkInRow(Board, 5, 0, 0). %bottom right cell
 
 
 
 
+%===============SIDE CHECK RULES=================
+% Side 1 
+isSide(R, C, SideNumber):-
+	R = 0,
+	SideNumber is 1.
+% Side 2 
+isSide(R, C, SideNumber):-
+	R = 1, !,
+	C = 5,
+	SideNumber is 2.
+isSide(R, C, SideNumber):-
+	R = 2, !,
+	C = 6,
+	SideNumber is 2.
+isSide(R, C, SideNumber):-
+	R = 3, !,
+	C = 7,
+	SideNumber is 2.
+% Side 6 
+isSide(R, C, SideNumber):-
+	C = 0, !,
+	R < 4, !,
+	R > 0,
+	SideNumber is 6.
+isSide(_, _, SideNumber):-
+	fail.
 
-%fazer para os restantes cantos
+% Side 1 (R, Index, C)
+checkSide(Board, 1, R, C):-
+	checkInRow(Board, R, 0, C+1). %right cell
+checkSide(Board, 1, R, C):-
+	checkInRow(Board, R, 0, C-1). %left cell
+checkSide(Board, 1, R, C):-
+	checkInRow(Board, R+1, 0, C+1). %bottom right cell
+checkSide(Board, 1, R, C):-
+	checkInRow(Board, R+1, 0, C). %bottom left cell
+% Side 2
+checkSide(Board, 2, R, C):-
+	checkInRow(Board, R-1, 0, C-1). %top cell
+checkSide(Board, 2, R, C):-
+	checkInRow(Board, R, 0, C-1). %top left cell
+checkSide(Board, 2, R, C):-
+	checkInRow(Board, R+1, 0, C). %left cell
+checkSide(Board, 2, R, C):-
+	checkInRow(Board, R+1, 0, C+1). %bottom cell
+% Side 6
+checkSide(Board, 6, R, C):-
+	checkInRow(Board, R-1, 0, C). %top cell
+checkSide(Board, 6, R, C):-
+	checkInRow(Board, R, 0, C+1). %right cell
+checkSide(Board, 6, R, C):-
+	checkInRow(Board, R+1, 0, C+1). %bottom right cell
+checkSide(Board, 6, R, C):-
+	checkInRow(Board, R+1, 0, C). %bottom left cell
 
 
 
-checkAdjacentCells(Row, Column):-
+%===============ADD PIECE=================
+
+
+checkAdjacentCells(Board, Row, Column):-
 	isCorner(Row, Column, CornerNumber),
-	checkCorner(ConerNumber).
-checkAdjacentCells(Row, Column):-
-	isBorder(Row, Column, BorderNumber),
-	checkBorder(BorderNumber, Row, Column).
-checkAdjacentCells(Row, Column):- %most comon case
-	checkMiddleCell(Row, Column).
+	checkCorner(Board, ConerNumber).
+checkAdjacentCells(Board, Row, Column):-
+	isSide(Row, Column, BorderNumber),
+	checkSide(Board, BorderNumber, Row, Column).
+checkAdjacentCells(Board, Row, Column):- %most comon case
+	checkMiddleCell(Board, Row, Column).
 
 
 
 tryToAddPieceToBoard(Color, Row, Column):-
-	checkAdjacentCells(Row, Column),
+	Row = 4, !,
+	Column = 8, !,
+	checkCorner(BoardState, 3),
 	addPieceToBoard(Color, Row, Column).
+tryToAddPieceToBoard(Color, Row, Column):-
+	Row = 4, !,
+	Column = 0, !,
+	checkCorner(BoardState, 6),
+	addPieceToBoard(Color, Row, Column).
+tryToAddPieceToBoard(Color, Row, Column):-
+	Row = 4, !,
+	checkMiddleCell(BoardState ,Row, Column),
+	addPieceToBoard(Color, Row, Column).
+tryToAddPieceToBoard(Color, Row, Column):-
+	Row < 4, !,
+	checkAdjacentCells(BoardState ,Row, Column),
+	addPieceToBoard(Color, Row, Column).
+tryToAddPieceToBoard(Color, Row, Column):-
+	Row > 4, !,
+	RevRow is 8 - Row,
+	reverse(BoardState, ReversedBoard),
+	checkAdjacentCells(ReversedBoard ,RevRow, Column),
+	addPieceToBoard(Color, Row, Column).
+
 
 
 
