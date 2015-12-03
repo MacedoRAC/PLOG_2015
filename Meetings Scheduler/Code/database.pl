40a7d1954194b56e46eca2b524a2c704aac966c4
mariomacedo <mario.macedo@ludustechnology.com>
2015-12-03 15:04:00 +0000

    few updates

--- Meetings Scheduler/Code/database.pl
+++ Meetings Scheduler/Code/database.pl
@@ -13,42 +13,42 @@ participantID(10).
 participantID(11).
 participantID(12).
 
-%Metting(name, duration)
-meeting(thesis_presentation).
-meeting(company_meeting).
-meeting(chairman_election).
+%Metting(id, name)
+meeting(1, thesis_presentation).
+meeting(2, company_meeting).
+meeting(3, chairman_election).
 
 %Meetings duration
-duration(thesis_presentation,1).
-duration(company_meeting,1).
-duration(chairman_election,3).
+duration(meeting(thesis_presentation),1).
+duration(meeting(company_meeting),1).
+duration(meeting(chairman_election),3).
 
 
 %obligatory and optional presences
 obligatory(participantID(12), meeting(thesis_presentation)).
 obligatory(participantID(3), meeting(thesis_presentation)).
 obligatory(participantID(5), meeting(thesis_presentation)).
-optional(participantID(1), meeting(thesis_presentation)).
-optional(participantID(2), meeting(thesis_presentation)).
 obligatory(participantID(10), meeting(company_meeting)).
 obligatory(participantID(3), meeting(company_meeting)).
-optional(participantID(5), meeting(company_meeting)).
 obligatory(participantID(1), meeting(chairman_election)).
 obligatory(participantID(2), meeting(chairman_election)).
 obligatory(participantID(3), meeting(chairman_election)).
+optional(participantID(1), meeting(thesis_presentation)).
+optional(participantID(2), meeting(thesis_presentation)).
+optional(participantID(5), meeting(company_meeting)).
 optional(participantID(4), meeting(chairman_election)).
 optional(participantID(5), meeting(chairman_election)).
 optional(participantID(6), meeting(chairman_election)).
 
 %Placement
-place(thesis_presentation,auditorium).
-place(company_meeting,normal).
-place(chairman_election,auditorium).
+place(meeting(thesis_presentation),auditorium).
+place(meeting(company_meeting),normal).
+place(meeting(chairman_election),auditorium).
 
-%Salas(Numero, Tipo) -> Regular=normal, Auditorium=tem projetor, Conference Room=videoconferencia
-sala(1,normal).
-sala(2,normal).
-sala(3,normal).
-sala(4,conference_room).
-sala(5,conference_room).
-sala(6,auditorium).
+%Salas(Numero, Tipo, capacidade) -> Regular=normal, Auditorium=tem projetor, Conference Room=videoconferencia
+sala(1,normal, 5).
+sala(2,normal, 5).
+sala(3,normal, 5).
+sala(4,conference_room, 10).
+sala(5,conference_room, 10).
+sala(6,auditorium, 15).
--- Meetings Scheduler/Code/scheduler.pl
+++ Meetings Scheduler/Code/scheduler.pl
@@ -4,4 +4,17 @@
 
 %variables
 %working shedule
-Time in 9..12\/14..20.
+schedule:-
+	length(Part, 12),
+	length(Meetings, 3),
+	length(Salas, 6),
+	length(Time, 9),
+
+	domain(Part, 1, 12), all_distinct(Part),
+	domain(Meetings, 1, 3), all_distinct(Meetings),
+	domain(Salas, 1, 6), all_distinct(Salas),
+	domain(Time, 1, 9), all_distinct(TIme),
+
+	duration(meeting(Meetings), Dur),
+	(Time + Dur) #< 9,
+	global_cardinality(Time, [])
